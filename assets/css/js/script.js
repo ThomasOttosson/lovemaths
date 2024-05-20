631193f5f10f4fc5dd62d6f46f261ec67240b3d9
ThomasOttosson Ottosson <tomasottosson1@gmail.com>
2024-05-20 17:41:38 +0000

    Add

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,5 +1,5 @@
-
-
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
@@ -9,16 +9,33 @@ document.addEventListener("DOMContentLoaded", function() {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
-                let gametype = this.getAttribute("data-type");
-                alert(`You clicked ${gametype}`);
-            }  
-        })
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
     }
-})
 
-function runGame() {
-let num1 = Math.floor(Math.random() * 25) + 1;
-let num2 = Math.floor(Math.random() * 25) + 1;
+    runGame("addition");
+
+});
+
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame(gameType) {
+
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
 
 function checkAnswer() {
@@ -27,6 +44,16 @@ function checkAnswer() {
 
 function calculateCorrectAnswer() {
 
+let operand1 = parseInt(document.getElementById('operand1').innerText);
+let operand2 = parseInt(document.getElementById('operand2').innerText);
+let operator = parseInt(document.getElementById('operator').innerText);
+
+if (operator === "+") {
+return [operand1 + operand2, "addition"]
+} else {
+    alert(`Unimplemented operator ${operator}`);
+    throw `Unimplemented operator ${operator}. Aborting!`;
+}
 }
 
 function incrementScore() {
@@ -37,8 +64,12 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
 }
 
 function displaySubtractQuestion() {
@@ -48,3 +79,5 @@ function displaySubtractQuestion() {
 function displayMultiplyQuestion() {
     
 }
+
+
